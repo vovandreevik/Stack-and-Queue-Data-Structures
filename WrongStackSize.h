36603275e96cbe41c2a6db744f7e613aa6b2bdc1
Vladimir Andreev <116523390+vovandreevik@users.noreply.github.com>
2023-10-26 22:27:31 +0300

    Add files via upload

---
+++ WrongStackSize.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <exception>
+
+class WrongStackSize : public std::exception
+{
+public:
+    const char* what() const noexcept
+    {
+        return "Wrong stack size";
+    }
+};
+
+class WrongQueueSize : public std::exception 
+{
+public:
+    const char* what() const noexcept
+    {
+        return "Wrong queue size!";
+    }
+};
